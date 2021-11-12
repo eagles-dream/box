bdfde136b75538b45ac9cd0c6ff4d9bcd5e640dd
eagles70 <jkkim70@gmail.com>
2021-11-12 23:19:32 +0900

    history.push를 이용하여 페이지 이동

--- src/components/data/data.jsx
+++ src/components/data/data.jsx
@@ -3,74 +3,86 @@ const data = [
     index: 1,
     id: '',
     icon: "fas fa-box",
-    title: '제품목록'
+    title: '제품목록',
+    status: false
   },
   {
     index: 2,
     id: 'enter',
     icon: "fas fa-arrow-down",
-    title: '입고'
+    title: '입고',
+    status: false
   },
   {
     index: 3,
     id: 'out',
     icon: "fas fa-arrow-up",
-    title: '출고'
+    title: '출고',
+    status: false
   },
   {
     index: 4,
     id: '',
     icon: "fas fa-exchange-alt",
-    title: '조정'
+    title: '조정',
+    status: false
   },
   {
     index: 5,
     id: 'enter',
     icon: "far fa-file-alt",
-    title: '구매 및 판매'
+    title: '구매 및 판매',
+    status: false
   },
   {
     index: 6,
     id: 'out',
     icon: "fas fa-history",
-    title: '히스토리'
+    title: '히스토리',
+    status: false
   }, 
   {
     index: 7,
     id: '',
     icon: "far fa-chart-bar",
-    title: '대시보드'
+    title: '대시보드',
+    status: false
   },
   {
     index: 8,
     id: 'enter',
     icon: "far fa-eye",
-    title: '재고분석'
+    title: '재고분석',
+    status: false
   },
   {
     index: 9,
     id: 'out',
     icon: "fas fa-plus-circle",
-    title: '추가기능'
+    title: '추가기능',
+    status: false
   },
   {
     index: 10,
     id: '',
     icon: "fas fa-database",
-    title: '데이터 관리'
+    title: '데이터 관리',
+    status: false
   },
   {
     index: 11,
     id: 'enter',
     icon: "fas fa-print",
-    title: '라벨인쇄'
+    title: '라벨인쇄',
+    status: false
   },
   {
     index: 12,
     id: 'out',
     icon: "fas fa-cogs",
-    title: '결제 및 설정'
-  },
+    title: '결제 및 설정',
+    status: false
+  }
 ]
 
 export default data;
