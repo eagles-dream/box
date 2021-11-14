f8595d1193a13b362023e2b78c4c56915cfdc672
eagles70 <jkkim70@gmail.com>
2021-11-14 16:57:54 +0900

    없는페이지 메시지 수정

--- src/components/body/errorPage/errorPage.jsx
+++ src/components/body/errorPage/errorPage.jsx
@@ -4,7 +4,9 @@ import styles from './errorPage.module.css'
 const ErrorPage = () => {
   return(
     <div className={styles.container}>
-      <span>페이지가 없습니다.</span>
+      <span>현재 작업중입니다.</span>
+      <span></span>
+      <span>상단과 좌측 메뉴버튼을 클릭해 보세요</span>
     </div>    
   )
 }
